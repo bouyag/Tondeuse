b7ec4dc1941590390280e33f66919974611876af
bouyag <azddin.smi@gmail.com>
2024-06-05 01:30:23 +0200

    second commit

--- src/main/java/fr/sg/tondeuse/repository/TondeuseRepository.java
+++ src/main/java/fr/sg/tondeuse/repository/TondeuseRepository.java
@@ -2,6 +2,8 @@ package fr.sg.tondeuse.repository;
 
 import fr.sg.tondeuse.entities.Tondeuse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TondeuseRepository extends JpaRepository<Tondeuse, Long> {
 }
